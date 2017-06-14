90ab4fe9d56e703c420fa89eefd902d3f5d32036
Petar Georgiev <petargeorgiev123@gmail.com>
2017-06-14 20:40:57 +0200

    add Login page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, session
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
+app.secret_key = 'why would I tell you my secret key?'
 mysql = MySQL()
 
 # MySQL configurations
@@ -33,8 +33,8 @@ def signUp():
     if _name and _email and _password:
         conn = mysql.connect()
         cursor = conn.cursor()
-        _hashed_password = generate_password_hash(_password)
-        cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+        # _hashed_password = generate_password_hash(_password)
+        cursor.callproc('sp_createUser', (_name, _email, _password))
         data = cursor.fetchall()
 
         if len(data) is 0:
@@ -47,6 +47,47 @@ def signUp():
     else:
         return json.dumps({'html': '<span>Enter the required fields</span>'})
 
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if data[0][3] == _password:
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error='Wrong Email address or Password.')
+        else:
+            return render_template('error.html', error='Wrong Email address or Password.')
+
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    finally:
+        cursor.close()
+        con.close()
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html', error='Unauthorized Access')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
