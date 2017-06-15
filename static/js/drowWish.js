6df22f7089fcf39aef6d3cae96b4e92e7e882ddc
Petar Georgiev <petargeorgiev123@gmail.com>
2017-06-15 22:53:45 +0200

    Add List

---
+++ static/js/drowWish.js
@@ -0,0 +1,17 @@
+$(function() {
+        $.ajax({
+            url: '/getWish',
+            type: 'GET',
+            success: function(res) {
+
+                // Parse the JSON response
+                var wishObj = JSON.parse(res);
+
+                // Append to the template
+                $('#listTemplate').tmpl(wishObj).appendTo('#ulist');
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
